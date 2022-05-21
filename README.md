349833059e60aae9ab06f3bf52fa72ae47671233
TzarHexonel <masaya-miki@live.com>
2022-05-21 19:56:07 +0200

    delete, scores and quit options available. Less error-prone.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # guesser_from_hell
 
 My first python project.
+It keeps getting upgraded, though.
 
-While studying Python I've came across a lot of number guessing programs, so I've decided to take it to the next level.
+When I started studying Python I came across a lot of number guessing projects, so I've decided to take it to the next level.
 This guesser will reward your hardwork, help you out when in need, and remember you when you revisit it.
 
 Have fun!
--- guesser_from_hell.py
+++ guesser_from_hell.py
@@ -3,57 +3,52 @@
 import random, json
 import functions as func
 
-print(
-"""###############################################################################################################
+print("""###############################################################################################################
 
                                 W  ・  E  ・  L  ・  C  ・  O  ・  M  ・  E
 
-###############################################################################################################""")
+###############################################################################################################
+    """)
 
-info = './difficulties.json'    # relative paths just save everything in the same folder
-data = './scores.json'          # otherwise use the absolute path
+info = 'difficulty_info.json'    # relative paths just save everything in the same folder
+saved = 'scores.json'          # otherwise use the absolute path
 with open(info) as f:
     diff_dict = json.load(f)
 
-users = {}                      # placeholder. Gets updated later
+mode = ''
+data = {}                      # placeholder. Gets updated later
 try:
-    with open(data, 'r') as r:
-        users = json.load(r)    # loads a file if it exists
+    with open(saved, 'r') as r:
+        data = json.load(r)    # loads a file if it exists
 except FileNotFoundError:
     pass
 name = input("What is your name?: ")
-if name in users:
-    print(f"\nWelcome back, {name}!")
+if name in data:
+    print(f"\nWelcome back, {name.title()}!"
+            f"\nLet's continue where you left off. Your current score is * {data[name]} *!")
 else:
-    print("\nI see this is your first time playing this. Have fun, let's see how good you are at guessing!")
-    users[name] = 0
+    print("\nI see this is your first time playing this.\nHave fun, let's see how good you are at guessing!")
+    data[name] = 0
 
-difficulties = ["baby", "medium", "impossible"]
-diff_input = func.choose_diff(difficulties)
 while True:
-    max_number = diff_dict[diff_input]["max"]
-    number = random.randint(1, max_number)      # generates a number
-    hints = func.create_hints(number, max_number, diff_input)   # and hints fitting wichever mode is chosen
+    mode = func.choose_diffs(mode, name, saved, data)
+    max_num = diff_dict[mode]['max']
+    number = random.randint(1, diff_dict[mode]['max'])
+    hints = func.create_hints(number, max_num, mode)   # and hints fitting wichever mode is chosen
+
     for i in range(len(hints)):
-        prompt = func.valid_number(input(hints[i]), max_number)
-        if prompt == number:
-            const = 3 - i
-            if i == 3:                  # want to reward that extra round in baby / medium mode
-                const = 1
-            print(diff_dict[diff_input]['messages'][str(i+1)])
-            users[name] += diff_dict[diff_input]['point'] * const
-            break           # this is it, if you've got it right
-        diff_dict[diff_input]['minus']
-        if diff_input == 'evil' or diff_input == 'satan':
-            number = random.randint(1, max_number)
-            hints = func.create_hints(number, max_number, diff_input)   # Because number changes each round, so do the hints
-        if i == len(hints)-1:       # Reveal the answer ONLY when the person missed every chance.
-            print(f"The number was {number}.")
-    if users[name] >= 15:       # evil and satan modes are available only when your score is 15 or higher
-        print("\nYou have gained a total of 15 Points. \nYou've UNLOCKED 'evil' and 'satan' modes!")
+        points = func.guess_points(mode, i, number, hints, saved, data)
+        data[name] += points
+        if points > 0:
+            print(diff_dict[mode]['msg'][str(i)])
+            break
+        elif mode == 'evil' or mode == 'satan':
+            number = random.randint(1, max_num)
+            hints = func.create_hints(number, max_num, mode)   # Because number changes each round, so do the hints
+        elif i == len(hints)-1:               # Reveal the answer ONLY when the person missed every chance.
+            print(f"You missed your shots.\nThe number was {number}!")
+    if data[name] >= 15 and data[name] - points < 15:
+        print("\nYou have gained more than 15 Points. You've unlocked 'evil' and 'satan' modes!")
         difficulties = ["baby", "medium", "impossible", "evil", "satan"]
-    else:
-        difficulties = ["baby", "medium", "impossible"]
-    print(f"\n ++++++++++ The current score is : {users[name]} ++++++++++") 
-    func.retry(users, data)     # saves the whole 'user' dict
-    diff_input = func.change_diff(difficulties, diff_input)
+    print(f"\n +++++++++++++++ The current score is : {data[name]} +++++++++++++++") 
+    func.retry(input("\nDo you want to try again? (yes/no): "), saved, data)
